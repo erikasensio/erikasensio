67591a627954f3f1f0fb9460393c5ac7b813a8e3
Erik Asensio <erikasensioperez@gmail.com>
2022-08-31 20:36:06 +0200

    ok

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -8,8 +8,16 @@ function About() {
         <div className="aboutContainer">
             <div className="aboutInfo">
                 <h1 className="aboutTitle">¿Who am I?</h1>
-                <p className="aboutText">Hello I'm Erik, I'm 18 years old and I consider myself constant and ambitious, I've always worked 100% on my projects and I've prepared myself as a Full Stack Developer in the best way I could, I started being self-taught at 15 years old.
-                    I love team-working, I think that being able to explain and make others understand is the best way to lay down a concept.</p>
+                <p className="aboutText">Hola, me llamo Erik Asensio y nací el 14/09/2004.
+
+Soy un chico constante y ambicioso, sobretodo en el mundo laboral. Siempre he dado el 100% para conseguir mis metas y creo que voy por buen camino. 
+Soy un chico divertido pero profesional, me encanta trabajar en equipo y sobretodo ayudar, considero que explicar es la mejor manera de asentar un concepto.
+
+Sin duda siempre he querido ser desarrollador, todo empezó con 14 años cuando conocí este mundo en el instituto y desde ese momento ya he hecho mil inventos para no perder el foco. He hecho muchísimos proyectos por mi cuenta con el fin de aprender, ya que hasta los 17 años no acabé mis estudios y he podido inscribirme en un Coding Bootcamp.
+
+A los 15 saqué mi lado emprendedor y comencé a trabajar como freelance, haciendo sitios web y e-commerce para pequeños negocios de la zona, incluso hice mis propias tarjetas ofreciéndome como desarrollador de puerta en puerta. Todo esto me sirvió para aprender por mi propia cuenta la importancia de todo lo que conlleva la parte digital de un negocio, no tan sólo para eso, sino que cada vez me gustaba más y aquí estoy, ofreciendome como Junior Full Stack Developer.
+
+Sigo el camino del MERN Stack, conocimientos sólidos en Javascript, MongoDB, NodeJS, React.js y React Native. Interesado en el TypeScript..</p>
                 <div className="aboutButtons">
                     <a href={englishCV} className="aboutButton">Download English CV</a>
                     <a href={spanishCV} className="aboutButton">Download Spanish CV</a>
