ba40dd83c93f680cf1816caf7d051b227214340c
Erik Asensio <erikasensioperez@gmail.com>
2022-09-01 16:19:17 +0200

    app changed

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Skills from "./components/Skills"
-import Portfolio from "./components/Portfolio"
+// import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 
@@ -15,7 +15,7 @@ function App() {
             <Hero />
             <About />
             <Skills /> 
-            <Portfolio />
+            {/* <Portfolio /> */}
             <Contact />
             <Footer />
         </div>
